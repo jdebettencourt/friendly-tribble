700d80f5802617b73491331dc399d098706bd4db
jdebettencourt <james.s.debettencourt@gmail.com>
2017-09-26 23:36:34 +0000

    catch-up Lab 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'my_pages/home'
+
+  get 'my_pages/help'
+
+  get 'my_pages/about'
+
+  resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#hello'
+  root 'users#index'
 
 
 end
